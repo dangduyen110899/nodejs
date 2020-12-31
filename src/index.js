e9761910294f93b3240913ad165dd32dbc242a4c
dangduyen110899 <duyendt@fimo.edu.vn>
2020-12-31 15:33:18 +0700

    [add] view

--- src/index.js
+++ src/index.js
@@ -7,19 +7,40 @@ const app = express();
 const port = 3000;
 
 // http logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname,'public')));
+
+// midleware from submitform send data
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// midleware from xmlhttprequest, fetch, axios, ajax send data
+app.use(express.json());
 
 // template engine
 app.engine('hbs', handlebars({
   extname: ".hbs"
 }));
 app.set('view engine', 'hbs');
-
 app.set('views',path.join(__dirname,'resources\\views'))
 
-app.get('/new', function (req, res) {
+// action -> dispatch -> function handler=controller
+// route
+app.get('/', function (req, res) {
   res.render('home');
+});
+
+app.get('/new', function (req, res) {
+  res.render('new');
+});
+
+app.get('/search', function (req, res) {
+  res.render('search');
+});
+
+app.post('/search', function (req, res) {
+  res.send(req.body);
 });
 
 app.listen(port, () => console.log("hehe"));
